bbf89c59c8503ccb1c043bbc910ff28dc24006ff
Phillip Tennen <phillip@axleos.com>
2023-10-26 19:41:21 +0100

    Chunks iterator optionally takes a step

--- gps/utils.py
+++ gps/utils.py
@@ -11,8 +11,13 @@ from functools import lru_cache, wraps
 _IterType = TypeVar("_IterType")
 
 
-def chunks(li: Collection[_IterType], chunk_size: int) -> Iterator[_IterType]:
-    for i in range(0, len(li), chunk_size):
+def chunks(li: Collection[_IterType], chunk_size: int, step: int | None = None) -> Iterator[_IterType]:
+    chunk_step = chunk_size
+    if step:
+        if step <= chunk_size:
+            raise ValueError(f'Expected the custom step to be at least a chunk size')
+        chunk_step = step
+    for i in range(0, len(li), chunk_step):
         yield li[i:i + chunk_size]
 
 
