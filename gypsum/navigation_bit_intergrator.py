7427bbe7966daac83d364265fd46c12eca8bec57
Phillip Tennen <phillip@axleos.com>
2023-12-05 11:00:00 +0000

    Refactor logic to select a bit phase

--- gypsum/navigation_bit_intergrator.py
+++ gypsum/navigation_bit_intergrator.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 
 from gypsum.antenna_sample_provider import ReceiverTimestampSeconds
 from gypsum.constants import PSEUDOSYMBOLS_PER_NAVIGATION_BIT
+from gypsum.constants import PSEUDOSYMBOLS_PER_SECOND
 from gypsum.events import Event
 from gypsum.tracker import BitValue, NavigationBitPseudosymbol
 from gypsum.utils import chunks
@@ -53,6 +54,78 @@ class NavigationBitIntegrator:
         self.queued_pseudosymbols: list[EmittedPseudosymbol] = []
         self.determined_bit_phase: int | None = None
         self.bit_index = 0
+    def _determine_bit_phase_from_queued_bits(self) -> list[Event]:
+        # Have we seen enough bits to determine the navigation bit phase?
+        # (The bound here can probably be lowered if useful)
+        # PT: What if we determine a bit phase too early, before we've locked, and it's wrong?
+        # Give ourselves 6 seconds to lock
+        seconds_count_to_buffer_symbols_before_attempting_phase_selection = 60
+        seconds_count_to_consider_for_symbol_phase_selection = 2
+        if len(self.queued_pseudosymbols) < (PSEUDOSYMBOLS_PER_SECOND * seconds_count_to_buffer_symbols_before_attempting_phase_selection):
+            # Not enough queued symbols to detect a bit phase
+            # _logger.info(
+            #    f'Pseudosymbol integrator hasn\'t yet determined bit phase '
+            #    f'but has only processed {len(self.queued_pseudosymbols)} pseudosymbols'
+            # )
+            return []
+
+        events = []
+        _logger.info(
+            f"Pseudosymbol integrator has seen enough bits ({len(self.queued_pseudosymbols)}), selecting a bit phase..."
+        )
+        # Look at the symbols from the final four seconds, as they'll probably be much closer to correct than the
+        # initial symbols we see.
+        # We'll need two seconds worth of symbols to select a phase, as we try different offsets up to 20 symbols
+        # This could instead be 1 second of we rolled instead of sliding the start.
+
+        # TODO(PT): This only considers one bit! Perhaps we should do multiple bits again
+        symbols_considered_for_phase_selection = self.queued_pseudosymbols[
+            PSEUDOSYMBOLS_PER_NAVIGATION_BIT * (
+                seconds_count_to_buffer_symbols_before_attempting_phase_selection -
+                seconds_count_to_consider_for_symbol_phase_selection
+            ):
+        ]
+        phase_guess_to_confidence_score = {}
+        for phase_guess in range(0, PSEUDOSYMBOLS_PER_NAVIGATION_BIT):
+            phase_guess_confidence = 0
+            symbols_grouped_into_bits = list(
+                chunks(symbols_considered_for_phase_selection[phase_guess:], PSEUDOSYMBOLS_PER_NAVIGATION_BIT)
+            )
+            for symbols_in_bit in symbols_grouped_into_bits:
+                bit_confidence = abs(sum([symbol.pseudosymbol.as_val() for symbol in symbols_in_bit]))
+                phase_guess_confidence += bit_confidence
+            # Normalize based on the number of bit groups we looked at
+            phase_guess_to_confidence_score[phase_guess] = phase_guess_confidence / len(
+                symbols_grouped_into_bits
+            )
+            # This could be sensitive to tracking errors in this particular second of processing...
+
+        highest_confidence_phase_offset = max(
+            phase_guess_to_confidence_score, key=phase_guess_to_confidence_score.get  # type: ignore
+        )
+        highest_confidence_score = phase_guess_to_confidence_score[highest_confidence_phase_offset]
+        # highest_confidence_phase_offset = int(np.argmax(confidence_scores))
+        # highest_confidence_score = confidence_scores[highest_confidence_phase_offset]
+        _logger.info(
+            f"Highest confidence phase offset: {highest_confidence_phase_offset}. "
+            f"Score: {highest_confidence_score}"
+        )
+
+        highest_confidence_as_percentage: Percentage = abs(
+            highest_confidence_score / PSEUDOSYMBOLS_PER_NAVIGATION_BIT
+        )
+
+        if highest_confidence_as_percentage >= 0.70:
+            self.determined_bit_phase = highest_confidence_phase_offset
+            # Discard queued symbols from the first partial symbol
+            self.queued_pseudosymbols = self.queued_pseudosymbols[self.determined_bit_phase:]
+            events.append(DeterminedBitPhaseEvent(highest_confidence_phase_offset))
+        else:
+            _logger.info(
+                f"Highest confidence bit phase was below confidence threshold: {highest_confidence_as_percentage}"
+            )
+            events.append(CannotDetermineBitPhaseEvent(highest_confidence_as_percentage))
+        return events
 
     def process_pseudosymbol(self, receiver_timestamp: ReceiverTimestampSeconds, pseudosymbol: NavigationBitPseudosymbol) -> list[Event]:
         events: list[Event] = []
@@ -62,70 +135,7 @@ class NavigationBitIntegrator:
         ))
 
         if self.determined_bit_phase is None:
-            # Have we seen enough bits to determine the navigation bit phase?
-            # (The bound here can probably be lowered if useful)
-            # PT: What if we determine a bit phase too early, before we've locked, and it's wrong?
-            # Give ourselves 6 seconds to lock
-            if len(self.queued_pseudosymbols) == (PSEUDOSYMBOLS_PER_NAVIGATION_BIT * 8):
-                _logger.info(
-                    f"Pseudosymbol integrator has seen enough bits ({len(self.queued_pseudosymbols)}), selecting a bit phase..."
-                )
-                # Look at the symbols from the final four seconds, as they'll probably be much closer to correct than the
-                # initial symbols we see.
-                # We'll need two seconds worth of symbols to select a phase, as we try different offsets up to 20 symbols
-                # This could instead be 1 second of we rolled instead of sliding the start.
-
-                # TODO(PT): This only considers one bit! Perhaps we should do multiple bits again
-                symbols_considered_for_phase_selection = self.queued_pseudosymbols[
-                    PSEUDOSYMBOLS_PER_NAVIGATION_BIT * 4:
-                ]
-                phase_guess_to_confidence_score = {}
-                for phase_guess in range(0, PSEUDOSYMBOLS_PER_NAVIGATION_BIT):
-                    phase_guess_confidence = 0
-                    symbols_grouped_into_bits = list(
-                        chunks(symbols_considered_for_phase_selection[phase_guess:], PSEUDOSYMBOLS_PER_NAVIGATION_BIT)
-                    )
-                    for symbols_in_bit in symbols_grouped_into_bits:
-                        bit_confidence = abs(sum([symbol.pseudosymbol.as_val() for symbol in symbols_in_bit]))
-                        phase_guess_confidence += bit_confidence
-                    # Normalize based on the number of bit groups we looked at
-                    phase_guess_to_confidence_score[phase_guess] = phase_guess_confidence / len(
-                        symbols_grouped_into_bits
-                    )
-                    # This could be sensitive to tracking errors in this particular second of processing...
-
-                highest_confidence_phase_offset = max(
-                    phase_guess_to_confidence_score, key=phase_guess_to_confidence_score.get    # type: ignore
-                )
-                highest_confidence_score = phase_guess_to_confidence_score[highest_confidence_phase_offset]
-                # highest_confidence_phase_offset = int(np.argmax(confidence_scores))
-                # highest_confidence_score = confidence_scores[highest_confidence_phase_offset]
-                _logger.info(
-                    f"Highest confidence phase offset: {highest_confidence_phase_offset}. "
-                    f"Score: {highest_confidence_score}"
-                )
-
-                highest_confidence_as_percentage: Percentage = abs(
-                    highest_confidence_score / PSEUDOSYMBOLS_PER_NAVIGATION_BIT
-                )
-
-                if highest_confidence_as_percentage >= 0.70:
-                    self.determined_bit_phase = highest_confidence_phase_offset
-                    # Discard queued symbols from the first partial symbol
-                    self.queued_pseudosymbols = self.queued_pseudosymbols[self.determined_bit_phase :]
-                    events.append(DeterminedBitPhaseEvent(highest_confidence_phase_offset))
-                else:
-                    _logger.info(
-                        f"Highest confidence bit phase was below confidence threshold: {highest_confidence_as_percentage}"
-                    )
-                    events.append(CannotDetermineBitPhaseEvent(highest_confidence_as_percentage))
-
-            else:
-                pass
-                # _logger.info(
-                #    f'Pseudosymbol integrator hasn\'t yet determined bit phase '
-                #    f'but has only processed {len(self.queued_pseudosymbols)} pseudosymbols'
-                # )
+            events.extend(self._determine_bit_phase_from_queued_bits())
 
         # We may have just determined the bit phase above, so check again
         if self.determined_bit_phase is not None:
