c25aaef1ec62f8e3497b022a3559312d2eb682a5
Phillip Tennen <phillip@axleos.com>
2023-12-17 19:59:51 +0000

    Add an auto-refreshing iframe to display tracker visualizers

--- web_dashboard/__init__.py
+++ web_dashboard/__init__.py
@@ -8,6 +8,8 @@ import os
 import falcon
 
 from web_dashboard.receiver_dashboard import GpsReceiverDashboard
+from web_dashboard.receiver_dashboard import GpsReceiverDashboardStateProvider
+from web_dashboard.receiver_dashboard import GpsReceiverDashboardTrackerVisualizers
 
 _logger = logging.getLogger(__name__)
 
@@ -21,10 +23,14 @@ _APP = falcon.App()
 # WSGI expects a global called `application`
 application = _APP
 
+# PT: Global state in lieu of a database...
+_STATE_PROVIDER = GpsReceiverDashboardStateProvider()
+
 
 def main():
     _logger.info(f'Starting up under gunicorn...')
-    _APP.add_route("/", GpsReceiverDashboard())
+    _APP.add_route("/", GpsReceiverDashboard(_STATE_PROVIDER))
+    _APP.add_route("/tracker_visualizers", GpsReceiverDashboardTrackerVisualizers(_STATE_PROVIDER))
 
 
 # Note that __name__ != __main__ here, since we're running via gunicorn
--- web_dashboard/receiver_dashboard.py
+++ web_dashboard/receiver_dashboard.py
@@ -9,7 +9,6 @@ import falcon
 from web_dashboard.messages import GpsReceiverState
 from web_dashboard.messages import SetCurrentReceiverStateRequest
 from web_dashboard.templates import JINJA_ENVIRONMENT
-from web_dashboard.templates import TemplateContext
 
 _logger = logging.getLogger(__name__)
 
@@ -24,32 +23,61 @@ class DashboardContext:
         self.__dict__.update(**kwargs)
 
 
+@dataclass
+class GpsReceiverDashboardStateProvider:
+    last_seen_gps_state: GpsReceiverState | None = None
+
+    def handle_state_update(self, state: GpsReceiverState) -> None:
+        self.last_seen_gps_state = state
+
+    def get_state(self) -> GpsReceiverState | None:
+        return self.last_seen_gps_state
+
+
 class GpsReceiverDashboard:
-    def __init__(self):
-        self.falcon_app = falcon.App()
-        self.resp = ""
-        self.last_seen_gps_state = None
+    def __init__(self, state_provider: GpsReceiverDashboardStateProvider):
+        self.state_provider = state_provider
 
     def on_get(self, _request: falcon.Request, response: falcon.Response) -> None:
-        _logger.info(f"Serving dashboard page {self.last_seen_gps_state}...")
+        _logger.info(f"Serving dashboard page...")
 
-        if self.last_seen_gps_state is None:
+        state = self.state_provider.get_state()
+        if state is None:
             response.text = "No data from receiver yet"
             return
 
         now = datetime.datetime.utcnow()
         context = DashboardContext(
             generated_at=now.strftime("%B %d, %Y at %H:%M"),
-            state=self.last_seen_gps_state,
+            state=state,
         )
-        print(context)
-        print(context.state)
         response.content_type = falcon.MEDIA_HTML
         response.text = JINJA_ENVIRONMENT.get_template("dashboard.html.jinja2").render(asdict(context))
 
-    def on_post(self, request: falcon.Request, response: falcon.Response) -> None:
-        _logger.info("Handling update from receiver...")
+    def on_post(self, request: falcon.Request, _response: falcon.Response) -> None:
+        # _logger.info("Handling update from receiver...")
 
         raw_post_data = json.loads(request.media)
         update = SetCurrentReceiverStateRequest(**raw_post_data)
-        self.last_seen_gps_state = update.current_state
+
+        # Push the update to the state storage
+        self.state_provider.handle_state_update(update.current_state)
+
+
+class GpsReceiverDashboardTrackerVisualizers:
+    def __init__(self, state_provider: GpsReceiverDashboardStateProvider):
+        self.state_provider = state_provider
+
+    def on_get(self, _request: falcon.Request, response: falcon.Response) -> None:
+        state = self.state_provider.get_state()
+        if state is None:
+            response.text = "No data from receiver yet"
+            return
+
+        now = datetime.datetime.utcnow()
+        context = DashboardContext(
+            generated_at=now.strftime("%B %d, %Y at %H:%M:%S"),
+            state=state,
+        )
+        response.content_type = falcon.MEDIA_HTML
+        response.text = JINJA_ENVIRONMENT.get_template("tracker_visualizers.html.jinja2").render(asdict(context))
