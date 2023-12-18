a7809ae534b2451d15990baeb34b9038474ea731
Phillip Tennen <phillip@axleos.com>
2023-12-18 00:14:44 +0000

    Present more data in the dashboard webapp

--- gypsum/radio_input.py
+++ gypsum/radio_input.py
@@ -286,7 +286,7 @@ INPUT_SOURCES = [
     #  (22, 9043.37046816484),
     #  (15, 12433.944236015503)]
     InputFileInfo.gnu_radio_recording_16x(path=_VENDORED_SIGNALS_ROOT / "test_hackrf_new3"),
-    InputFileInfo.gnu_radio_recording_2x(path=_VENDORED_SIGNALS_ROOT / "test_rtl_night"),
+    InputFileInfo.gnu_radio_recording_2x(path=_VENDORED_SIGNALS_ROOT / "test_rtl_night", utc_start_time=datetime.datetime.fromtimestamp(1702851337.874580792)),
 ]
 
 
--- web_dashboard/messages/__init__.py
+++ web_dashboard/messages/__init__.py
@@ -2,12 +2,16 @@ import pydantic
 
 from gypsum.antenna_sample_provider import ReceiverTimestampSeconds
 from gypsum.gps_ca_prn_codes import GpsSatelliteId
+from gypsum.world_model import OrbitalParameters
 
 
 class GpsReceiverState(pydantic.BaseModel):
     receiver_timestamp: ReceiverTimestampSeconds
     satellite_ids_eligible_for_acquisition: list[GpsSatelliteId]
     dashboard_figures: list[str]
+    tracked_satellite_count: int
+    processed_subframe_count: int
+    satellite_ids_to_orbital_parameters: dict[GpsSatelliteId, OrbitalParameters]
 
 
 class SetCurrentReceiverStateRequest(pydantic.BaseModel):
